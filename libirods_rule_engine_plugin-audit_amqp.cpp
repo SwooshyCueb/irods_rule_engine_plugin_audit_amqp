63713215cb4a3bbea460cb69c787c64223ebfcac
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2022-09-20 14:58:20 -0400

    [103] do not stringify pid

--- libirods_rule_engine_plugin-audit_amqp.cpp
+++ libirods_rule_engine_plugin-audit_amqp.cpp
@@ -202,8 +202,6 @@ namespace
 		char host_name[MAX_NAME_LEN];
 		gethostname(host_name, MAX_NAME_LEN);
 
-		pid_t pid = getpid();
-
 		std::string msg_str;
 		std::string log_file;
 
@@ -212,7 +210,10 @@ namespace
 			json_obj["@timestamp"] = time_ms;
 
 			insert_or_parse_as_bin(json_obj, "hostname", host_name);
-			json_obj["pid"] = std::to_string(pid);
+
+			pid_t pid = getpid();
+			json_obj["pid"] = pid;
+
 			json_obj["action"] = "START";
 
 			if (test_mode) {
@@ -268,7 +269,7 @@ namespace
 			json_obj["hostname"] = host_name;
 
 			pid_t pid = getpid();
-			json_obj["pid"] = std::to_string(pid);
+			json_obj["pid"] = pid;
 
 			json_obj["action"] = "STOP";
 
@@ -356,9 +357,7 @@ namespace
 			gethostname(host_name, MAX_NAME_LEN);
 			insert_or_parse_as_bin(json_obj, "hostname", host_name);
 
-			pid_t pid = getpid();
-			json_obj["pid"] = std::to_string(pid);
-
+			json_obj["pid"] = getpid();
 			json_obj["rule_name"] = _rn;
 
 			for (const auto& itr : _ps) {
